6398dc5ac8389de1f102a51916d47ca94dc74183
Abhi <abhidashing.singh@gmail.com>
2020-07-30 13:08:09 +0530

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./Pages/Home";
+import Post from "./Pages/Post";
+import Comment from "./Pages/Comment";
+import Header from "./components/Header";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/post">
+            <Post />
+          </Route>
+          <Route path="/comment">
+            <Comment />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
