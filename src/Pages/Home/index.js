4da5aa7f4cdc307135913e425dc711df388d1c93
Abhi <abhidashing.singh@gmail.com>
2020-07-30 16:14:20 +0530

    add Home Page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    return <h1>Home</h1>;
+  return (
+    <Jumbotron>
+      <h1>Hacker News</h1>
+      <p>
+        This is the React port of basic features from{" "}
+        <a href="https://news.ycombinator.com/">Hacker News</a>.
+        <br />
+        Please refer to the{" "}
+        <a href="https://github.com/HackerNews/API">api documentation</a> for
+        more details.
+      </p>
+      <p>
+        <Link className="btn btn-primary" to="/post/top">
+          Top Posts
+        </Link>
+      </p>
+    </Jumbotron>
+  );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
 
 export default function Header() {
-    return <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/post">Post</Link>
-    </li>
-    <li>
-      <Link to="/comment">Comment</Link>
-    </li>
-  </ul>;
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="/">
+        <img
+          alt=""
+          src="https://news.ycombinator.com/y18.gif"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{" "}
+        Hacker News
+      </Navbar.Brand>
+      <Navbar.Collapse className="justify-content-end">
+        <Nav activeKey="/home">
+          <Nav.Item>
+            <Link className="nav-link active" to="/">Home</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link className="nav-link" to="/post/top">Top</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link className="nav-link" to="/post/new">New</Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
