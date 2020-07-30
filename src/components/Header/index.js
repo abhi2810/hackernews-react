8b73b41d3453483810bc58fe78a0d55d0a442bd8
Abhi <abhidashing.singh@gmail.com>
2020-07-30 16:28:52 +0530

    add header and clsx

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { Link, useLocation } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap";
 
 export default function Header() {
+  let location = useLocation();
+  let [selected, setSelected] = React.useState("");
+
+  React.useEffect(() => {
+    console.log(location.pathname);
+    switch (location.pathname) {
+      case "/post/top":
+        setSelected("top");
+        break;
+      case "/post/new":
+        setSelected("new");
+        break;
+      default:
+        setSelected("");
+    }
+  }, [location]);
+
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand href="/">
@@ -18,13 +36,28 @@ export default function Header() {
       <Navbar.Collapse className="justify-content-end">
         <Nav activeKey="/home">
           <Nav.Item>
-            <Link className="nav-link active" to="/">Home</Link>
+            <Link
+              className={clsx("nav-link", selected === "" && "active")}
+              to="/"
+            >
+              Home
+            </Link>
           </Nav.Item>
           <Nav.Item>
-            <Link className="nav-link" to="/post/top">Top</Link>
+            <Link
+              className={clsx("nav-link", selected === "top" && "active")}
+              to="/post/top"
+            >
+              Top
+            </Link>
           </Nav.Item>
           <Nav.Item>
-            <Link className="nav-link" to="/post/new">New</Link>
+            <Link
+              className={clsx("nav-link", selected === "new" && "active")}
+              to="/post/new"
+            >
+              New
+            </Link>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
