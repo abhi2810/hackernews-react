570e552269691a4d37512fb14790dd875c9bc500
Abhi Singh <abhi.s@browserstack.com>
2020-11-23 03:40:01 +0530

    update snapshot.js

--- snapshot.js
+++ snapshot.js
@@ -12,7 +12,9 @@ PercyScript.run(async (page, percySnapshot) => {
     await percySnapshot('Hacker News home page');
 
     await page.click('#root > div > div > p:nth-child(3) > a');
+    await page.waitFor('#root > div > div > ul > li:nth-child(1) > div');
     await percySnapshot('Hacker News with a top Posts', { widths: [768, 992, 1200] });
     await page.click('#root > div > nav > div > div > div:nth-child(3) > a');
+    await page.waitFor('#root > div > div > ul > li:nth-child(1) > div');
     await percySnapshot('Hacker News with a new Posts', { widths: [768, 992, 1200] });
 });
