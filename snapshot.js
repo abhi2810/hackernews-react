1e4d0981663871083ede6433d4576e3faa98a238
Abhi Singh <abhi.s@browserstack.com>
2020-11-23 03:33:16 +0530

    test fix

--- snapshot.js
+++ snapshot.js
@@ -1,13 +1,10 @@
 const PercyScript = require('@percy/script');
-const httpServer = require('http-server');
 
 const PORT = process.env.PORT_NUMBER || 3000;
 const TEST_URL = `http://localhost:${PORT}`;
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
-    let server = httpServer.createServer();
-    server.listen(PORT);
 
     console.log(`Server started at ${TEST_URL}`);
 
@@ -18,5 +15,4 @@ PercyScript.run(async (page, percySnapshot) => {
     await percySnapshot('Hacker News with a top Posts', { widths: [768, 992, 1200] });
     await page.click('#root > div > nav > div > div > div:nth-child(3) > a');
     await percySnapshot('Hacker News with a new Posts', { widths: [768, 992, 1200] });
-    server.close();
 });
