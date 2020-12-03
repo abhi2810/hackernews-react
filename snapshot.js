4fb51fb4b0da1674df4a1453523e441d516b45dc
Abhi Singh <abhi.s@browserstack.com>
2020-12-03 19:42:25 +0530

    changes

--- snapshot.js
+++ snapshot.js
@@ -11,10 +11,10 @@ PercyScript.run(async (page, percySnapshot) => {
     await page.goto(TEST_URL);
     await percySnapshot('Hacker News home page');
 
-    await page.click('#root > div > div > p:nth-child(3) > a');
-    await page.waitFor('#root > div > div > ul > li:nth-child(1) > div');
-    await percySnapshot('Hacker News with a top Posts', { widths: [768, 992, 1200] });
-    await page.click('#root > div > nav > div > div > div:nth-child(3) > a');
-    await page.waitFor('#root > div > div > ul > li:nth-child(1) > div');
-    await percySnapshot('Hacker News with a new Posts', { widths: [768, 992, 1200] });
+    // await page.click('#root > div > div > p:nth-child(3) > a');
+    // await page.waitFor('#root > div > div > ul > li:nth-child(1) > div');
+    // await percySnapshot('Hacker News with a top Posts', { widths: [768, 992, 1200] });
+    // await page.click('#root > div > nav > div > div > div:nth-child(3) > a');
+    // await page.waitFor('#root > div > div > ul > li:nth-child(1) > div');
+    // await percySnapshot('Hacker News with a new Posts', { widths: [768, 992, 1200] });
 });
